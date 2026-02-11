2d255466097e1d6c56b46dbf3f92ff7bce63b450
otozumbadze <zuoto3647@gmail.com>
2026-02-11 15:48:18 +0400

    second commit

--- level 06/classwork/index.py
+++ level 06/classwork/index.py
@@ -1,2 +1,38 @@
 age = 14
 print(age)
+
+
+
+# input-ით ჩვენ მომხმარებელს ვატანინებთ ინფორმაციას (ეს არის შემავალი ინფორმაცია).
+
+# output-ით ჩვენ მომხმარებელს ვატანინებთ ინფორმაციას (ეს არის გამომავალი ინფორმაცია).
+
+
+
+age = 15
+
+age1 = 30
+
+print(age + age1)
+print(age - age1)
+print(age * age1)
+print(age / age1)
+
+
+
+
+fav_color = input("enter your favorite color: ")
+fav_Animal = input("enter your favorite animal: ")
+fav_place = input("enter your favorite place: ")
+
+
+print(f"my favorite color is {fav_color}. my favorite animal is {fav_Animal}. my favorite place is {fav_place}.")
+
+
+
+fav_car = input("my favorite car is: ")
+car = input("enter your second car: ")
+car1 = input("enter your third car: ")
+car2 = input("enter your fourth car: ")
+
+print(f"my favorite car is {fav_car}. just cars that i dont like so much {car}, {car1}, {car2}. ")
