7b7b02eea4e9b0c417786d14bd3f99f534ef4a89
otozumbadze <zuoto3647@gmail.com>
2026-02-11 21:36:49 +0400

    second commit

--- level 08/classwork/idex.py
+++ level 08/classwork/idex.py
@@ -1,1 +1,26 @@
+# 1#
 
+name = input("enter your name: ")
+
+print(name)
+
+
+bob = int(input("enter first letter: "))
+fox = int(input("enter second letter: "))
+
+print(bob + fox)
+
+
+age = (input("enter your age: "))
+
+print(int(age))
+
+
+
+a = input("enter your name: ")
+b = int(input("enter your age: "))
+c = float(input("enter your letter: "))
+
+print(a)
+print(b)
+print(c)
