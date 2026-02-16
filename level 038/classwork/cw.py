36ef12726f08f5112196750fdcbfc72fd90dc254
otozumbadze <zuoto3647@gmail.com>
2026-02-16 21:01:45 +0400

    second commit

---
+++ level 038/classwork/cw.py
@@ -0,0 +1,15 @@
+sentence = input("შეიყვანეთ წინადადება: ")
+
+a = sentence.split(' ')
+
+print(a)
+
+
+
+b = ['hi', 'hello', '84', '16']
+
+c = input("შეიყვანეთ სიმბოლო: ")
+
+result = c.join(b)
+
+print(result)
