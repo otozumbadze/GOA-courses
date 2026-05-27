66565759aa1a93331bf60abfde20d668485fa5de
otozumbadze <zuoto3647@gmail.com>
2026-05-27 20:52:28 +0400

    second commit

---
+++ level 14/homework/hw.py
@@ -0,0 +1,16 @@
+# Flowchart - არის დიაგრამა რომელიც გვაჩვენებს თუ რა პროცესი როგორ მიმდინარეობს.
+
+# Sequencing - არის თანმიმდევრობა რომელიც ზემოდან-ქვემოთ დაწერილი კოდი გაეშვება თანმიმდევრულად.
+
+# Selection - არის ის რასაც პროგრამა ირჩევს პირველს.
+
+# Iteration - არის ის რომ კოდი მრავალჯერ მეორდება.
+
+
+C = int(input("Enter your graduse: "))
+F = (C * 9/5) + 32
+print(F)
+
+
+sentance = "Niaisreallygoodteacher"
+print(sentance[0:3])
